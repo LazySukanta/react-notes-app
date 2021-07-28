620a8375fa707bdd5b9d9da222c9ab996438ed0c
Sukanta Dinda <dindasukanta19@gmail.com>
2021-07-28 18:40:45 +0530

    first commit

---
+++ src/components/Note.js
@@ -0,0 +1,20 @@
+// import { MdDeleteForever } from 'react-icons/md';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+
+const Note = ({ id, text, date, handleDeleteNote }) => {
+	return (
+		<div className='note'>
+			<span>{text}</span>
+			<div className='note-footer'>
+				<small>{date}</small>
+				<DeleteForeverIcon
+					onClick={() => handleDeleteNote(id)}
+					className='delete-icon'
+					size='1.3em'
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Note;
